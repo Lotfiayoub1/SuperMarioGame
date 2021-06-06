87503db2647f6ab9825db2be370190cbdb40e9de
amineouba <63561311+amineouba@users.noreply.github.com>
2021-06-06 21:44:20 +0100

    SpeedTest and EndMenuTest try

--- src/test/EndMenuTest.java
+++ src/test/EndMenuTest.java
@@ -1,32 +1,30 @@
 package test;
 
-import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertFalse;
 
 import java.awt.Color;
 import java.awt.DisplayMode;
 import java.awt.Font;
 import java.awt.Window;
 
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import com.TETOSOFT.graphics.ScreenManager;
 import com.TETOSOFT.test.EndMenu;
 
 public class EndMenuTest {
-	
-	private static EndMenu endmenu;
-	private static ScreenManager screen;
 
-	
+	private EndMenu endmenu;
+	private ScreenManager screen;
 
-	@BeforeAll
-    static void beforeAll() {
+	@BeforeEach
+	public void before() {
 		screen = new ScreenManager();
-		DisplayMode POSSIBLE_MODES[] = { new DisplayMode(800, 600, 32, 0),
-					new DisplayMode(800, 600, 16, 0), new DisplayMode(800, 600, 24, 0), new DisplayMode(640, 480, 16, 0),
-					new DisplayMode(640, 480, 32, 0), new DisplayMode(640, 480, 24, 0), new DisplayMode(1024, 768, 16, 0),
-					new DisplayMode(1024, 768, 32, 0), new DisplayMode(1024, 768, 24, 0), };
+		DisplayMode POSSIBLE_MODES[] = { new DisplayMode(800, 600, 32, 0), new DisplayMode(800, 600, 16, 0),
+				new DisplayMode(800, 600, 24, 0), new DisplayMode(640, 480, 16, 0), new DisplayMode(640, 480, 32, 0),
+				new DisplayMode(640, 480, 24, 0), new DisplayMode(1024, 768, 16, 0), new DisplayMode(1024, 768, 32, 0),
+				new DisplayMode(1024, 768, 24, 0), };
 
 		DisplayMode displayMode = screen.findFirstCompatibleMode(POSSIBLE_MODES);
 		screen.setFullScreen(displayMode);
@@ -36,33 +34,32 @@ public class EndMenuTest {
 		window.setBackground(Color.BLACK);
 		window.setForeground(Color.WHITE);
 
-		endmenu = new EndMenu(5,screen);
-    }
-	
-	@Test
-	void testExit() {
-		
-		new Thread(
-				()->{try {
-						Thread.sleep(3000);
-					} catch (InterruptedException ex) {
-						ex.printStackTrace();
-					}
-		
-					endmenu.getExit().press();
-					try {
-						Thread.sleep(3000);
-					} catch (InterruptedException ex) {
-						ex.printStackTrace();
-					}
-					assertNull(screen);
-					
-					 }
-		).start();
-		
-		endmenu.run();		
-		
+		endmenu = new EndMenu(5, screen);
 	}
-	
+
+	/*
+	 * @Test void testExit() {
+	 * 
+	 * new Thread( ()->{try { Thread.sleep(3000); } catch (InterruptedException ex)
+	 * { ex.printStackTrace(); }
+	 * 
+	 * endmenu.getExit().press(); try { Thread.sleep(3000); } catch
+	 * (InterruptedException ex) { ex.printStackTrace(); } assertNull(screen);
+	 * 
+	 * } ).start();
+	 * 
+	 * endmenu.run();
+	 * 
+	 * }
+	 */
+
+	@Test
+	public void testExit() {
+
+		endmenu.getExit().press();
+		endmenu.run();
+		assertFalse(endmenu.isWaiting());
+
+	}
 
 }
